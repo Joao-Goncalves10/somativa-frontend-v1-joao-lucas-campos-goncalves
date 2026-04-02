e4003bd9347b2857677ef7da323bbc3897f48eea
Joao-Goncalves10 <joao.l.goncalves@aluno.senai.br>
2026-04-02 14:00:12 -0300

    Resolução da Missão 1

---
+++ src/scripts/script.js
@@ -0,0 +1,13 @@
+/* MISSÃO 1: RECEPÇÃO DO ATLETA (DATE E CONDICIONAIS) */
+
+const agora = new Date()
+const hora = agora.getHours()
+const saudacao = document.getElementById('saudacao-atleta')
+
+if (hora < 12 && hora > 5) {
+    saudacao.textContent = 'Treino matinal liberado!'
+} if (hora >= 12 && hora <= 18) {
+    saudacao.textContent = 'Boa tarde, foco no treino!'
+} if (hora > 18) {
+    saudacao.textContent = 'Descanso noturno merecido!'
+}
