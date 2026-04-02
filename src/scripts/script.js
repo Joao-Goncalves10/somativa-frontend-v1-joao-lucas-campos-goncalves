2d535d842576aa7d9e6e41a928181dfff5b3e076
Joao-Goncalves10 <joao.l.goncalves@aluno.senai.br>
2026-04-02 14:43:54 -0300

    Resolução da Missão 3

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
-/* MISSÃO 1: RECEPÇÃO DO ATLETA (DATE E CONDICIONAIS) */
+// MISSÃO 1: RECEPÇÃO DO ATLETA (DATE E CONDICIONAIS) //
 const agora = new Date()
 const hora = agora.getHours()
 const saudacao = document.getElementById('saudacao-atleta')
@@ -12,7 +12,7 @@ if (hora < 12 && hora > 5) {
 }
 
 
-/* MISSÃO 2: BANNER MOTIVACIONAL (EVENTOS DE MOUSE) */
+// MISSÃO 2: BANNER MOTIVACIONAL (EVENTOS DE MOUSE) //
 const foco = document.getElementById('banner-foco')
 foco.addEventListener('mouseover', function() {
     foco.classList.add('foco-total')
@@ -20,4 +20,14 @@ foco.addEventListener('mouseover', function() {
 
 foco.addEventListener('mouseout', function() {
     foco.classList.remove()
+})
+
+
+// MISSÃO 3: CALCULADORA DE HIDRATAÇÃO (INPUT COM TRATAMENTO)
+const pesoAluno = document.getElementById('peso-aluno')
+const metaAgua = document.getElementById('meta-agua')
+pesoAluno.addEventListener('input', () => {
+    const valor = Number(pesoAluno.value) * 35
+
+    metaAgua.textContent = `Sua meta diária de água é de:${valor.toFixed(2)}ml por dia!`
 })
